bdd0f0477ef402c80ef1d5b3c031aa7271e21080
Nicolas <nicolaskruiz@gmail.com>
2022-10-26 00:21:57 -0300

    setTimeout agregado, falta agregar musica

--- js/index.js
+++ js/index.js
@@ -1,9 +1,28 @@
+let btnComenzar;
+let contenido;
+
+btnComenzar = document.getElementById('btnComenzar');
+contenido = document.getElementById('contenido');
+
+btnComenzar.addEventListener("click",btnComenzarClic);
 
 
-    let btnComenzar;
-    btnComenzar = document.getElementById('btnComenzar');
-    btnComenzar.addEventListener("click",btnComenzarClic);
-    function btnComenzarClic(){
-        
-    }
-    
+function btnComenzarClic(){
+    let terminacion1 = ["Presento","Atento","Intento","Reviento"]
+    let terminacion2 = ["Aparenta","Inventa","Noventa", "Incrementa"]
+
+    const t1 = setTimeout(()=>{
+        contenido.innerHTML = terminacion1[0] +"<br>"+ terminacion1[1] +"<br>"+ terminacion1[2] +"<br>"+ terminacion1[3];
+    },1000);
+    setTimeout(()=>{
+        clearInterval(t1)
+    },4000);
+
+
+    const t2 = setTimeout(()=>{
+        contenido.innerHTML = terminacion2[0] + "<br>" + terminacion2[1] + "<br>" + terminacion2[2] + "<br>" + terminacion2[3]  
+    },5000)
+
+
+
+}
