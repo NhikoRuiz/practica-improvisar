d078cd37a3d637f741de4cfa309844129f17a4a1
Nicolás <nicolaskruiz@gmail.com>
2023-05-23 18:29:52 +0100

    agregué la instrumental, falta arreglar el tiempo con las palabras

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 let btnComenzar = document.getElementById('btnComenzar');
 let contenido = document.getElementById('contenido');
+// audio
+let sound = new Audio('audios/bombap.mp3')
+
+
 
 
 
@@ -8,38 +12,39 @@ let terminacion2 = ["Aparenta","Inventa","Noventa", "Incrementa"];
 let terminacion3 = ["Conjuro","Muro","Perduro","Puro"];
 
 
+
 btnComenzar.addEventListener("click",()=>{
+    sound.play()
     contenido.style.fontFamily = 'Gemunu Libre';
     contenido.style.fontSize = '30px';
+    
+    
 
     const fraseComienzo = setTimeout(()=>{
         contenido.innerHTML = 'Comienza en';
-    },0000)
+    },23000)
     const num3 = setTimeout((e)=>{
         contenido.innerHTML= '3';
-    },500) 
+    },43000) 
     const num2 = setTimeout((e)=>{
         contenido.innerHTML = '2';
-    },1000) 
+    },25000) 
     const num1 = setTimeout((e)=>{
         contenido.innerHTML = '1';
-    },1500) 
+    },26000) 
 
     const t1 = setTimeout(()=>{
         contenido.innerHTML = terminacion1[0] +"<br>"+ terminacion1[1] +"<br>"+ terminacion1[2] +"<br>"+ terminacion1[3];
-    },2000);
-    setTimeout(()=>{
-        clearInterval(t1)
-    },4000);
+    },27000);
 
 
     const t2 = setTimeout(()=>{
         contenido.innerHTML = terminacion2[0] + "<br>" + terminacion2[1] + "<br>" + terminacion2[2] + "<br>" + terminacion2[3]  
-    },5000)
+    },38000)
 
     const t3 = setTimeout(()=>{
         contenido.innerHTML = terminacion3[0] + "<br>" + terminacion3[1] + "<br>" + terminacion3[2] + "<br>" + terminacion3[3]  
-    },10000)
+    },47000)
 
 });
 
